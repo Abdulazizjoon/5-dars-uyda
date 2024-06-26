483135166bf7eda6a11ec53c614dd9c3cd15a287
abdulaziz <abdulaziz@gmail.com>
2024-06-26 13:24:53 +0500

    ..

--- js/my.js
+++ js/my.js
@@ -1,6 +1,11 @@
 let kilc=document.querySelector('#flexSwitchCheckDefault')
 let a=document.querySelector('.a')
 let b=document.querySelector('.b')
+let al=document.querySelector('.al')
+let til1=document.querySelector('.light')
+let dark=document.querySelector('.dark')
+let unk=document.querySelector('.unk')
+let redy=document.querySelector('.redy')
 kilc.addEventListener('click',function() {
     b.style.display="none"
   a.style.marginLeft ="200px"
@@ -11,3 +16,10 @@ function salom() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+al.addEventListener('click',function() {
+  dark.textContent="темный режим"
+  til1.textContent="световой режим"
+  unk.textContent="Раскройте силу творчества с Figma!"
+  redy.textContent="Готовы улучшить свою дизайнерскую игру?"
+  but.textContent="Попробуйте Фигму прямо сейчас"
+})
